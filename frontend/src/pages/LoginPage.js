8c5216d6ef1b9c0341f3321e20fe3b420e93f6b4
Kevin Liu <kevinliusingapore@gmail.com>
2022-07-23 23:26:39 +0800

    minor document changes to default bootstrapped react
    feature: changed icons

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,6 +1,5 @@
 import LoginForm from '../components/forms/LoginForm';
 import { Fragment } from 'react';
-import Card from '../components/ui/Card';
 
 const LoginPage = () => {
   return (
--- frontend/src/pages/SignUpPage.js
+++ frontend/src/pages/SignUpPage.js
@@ -1,6 +1,5 @@
 import SignUpForm from '../components/forms/SignUpForm';
 import { Fragment } from 'react';
-import Card from '../components/ui/Card';
 
 const SignUpPage = () => {
   return (
